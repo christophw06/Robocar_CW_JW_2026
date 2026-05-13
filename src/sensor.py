8dbf9e7911fcba6b013a042bdd8034e28e20b2de
christophw06 <christoph.weiss.bw@gmail.com>
2026-05-13 16:43:20 +0200

    sensor program right/left/middle

--- src/sensor.py
+++ src/sensor.py
@@ -1,15 +1,29 @@
 from gpiozero import LineSensor
 from time import sleep
 
-sensor = LineSensor(23)
+linesensor_right = LineSensor(23)
+linesensor_mid = LineSensor(15)
+linesensor_left = LineSensor(14)
 
-def detected():
+def detected_right_line():
     print("Linie erkannt")
 
-def not_detected():
+def not_detected_right_line():
+    print("Keine Linie")
+    
+def detected_mid_line():
+    print("Linie erkannt")
+
+def not_detected_mid_line():
+    print("Keine Linie")
+    
+def detected_left_line():
+    print("Linie erkannt")
+
+def not_detected_left_line():
     print("Keine Linie")
 
-sensor.when_line = detected
-sensor.when_no_line = not_detected
+linesensor_right.when_line = detected
+linesensor_right.when_no_line = not_detected
 
 sleep(3)
