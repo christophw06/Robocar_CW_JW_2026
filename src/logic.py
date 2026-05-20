c8d6e812f39270dd8c52a7998b4cda1d4f5eaffb
christophw06 <christoph.weiss.bw@gmail.com>
2026-05-20 16:59:14 +0200

    Logic wheels

--- src/logic.py
+++ src/logic.py
@@ -1,17 +1,14 @@
 import motor
 import time
 
+motor.init()
+
 def stop_all_wheels():
-    motor.init()
     motor.front_left(0)
     motor.front_right(0)
     motor.rear_left(0)
     motor.rear_right(0)
 
-def wheel_front_left():
-    motor.init()
-    motor.front_left(70)
 
-wheel_front_left()
 time.sleep(3)
 stop_all_wheels()
