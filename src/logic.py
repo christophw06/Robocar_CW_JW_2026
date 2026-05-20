e1dfb1f6cf77a514b9497b7ab6a03ff50f70c215
christophw06 <christoph.weiss.bw@gmail.com>
2026-05-20 16:46:59 +0200

    Logic Stop engine, basic start

--- src/logic.py
+++ src/logic.py
@@ -1,4 +1,17 @@
 import motor
+import time
+
+def stop_all_wheels():
+    motor.init()
+    motor.front_left(0)
+    motor.front_right(0)
+    motor.rear_left(0)
+    motor.rear_right(0)
 
 def wheel_front_left():
-    main.init()
+    motor.init()
+    motor.front_left(70)
+
+wheel_front_left()
+time.sleep(3)
+stop_all_wheels()
