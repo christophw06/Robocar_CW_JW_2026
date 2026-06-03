7ae7ccc7d50cba27a579aff7a580ae0b6f5e71b6
christophw06 <christoph.weiss.bw@gmail.com>
2026-06-03 13:39:32 +0200

    Queue supplemented -> threading with one list

--- src/pid.py
+++ src/pid.py
@@ -1,9 +1,10 @@
+import queue
 import threading
 import time
 
 import sensor
 
-sensor_values = []
+sensor_values = queue.Queue()
 
 
 def get_value_sensor_in_list(
@@ -15,28 +16,33 @@ def get_value_sensor_in_list(
     global sensor_values
     while True:
         sensor_value_of_func = sensor_function(sensor_orientation)
-        number_of_values = len(sensor_values)
+        number_of_values = sensor_values.qsize()
         if sensor_value_of_func:
             sensor_value_converted = 1
         else:
             sensor_value_converted = 0
 
         if number_of_values < number_of_values_to_readin:
-            sensor_values.append(sensor_value_converted)
+            sensor_values.put(sensor_value_converted)
             time.sleep((readin_timestep_in_ms / number_of_values_to_readin) / 100)
         elif number_of_values >= number_of_values_to_readin:
-            del sensor_values[0]
-            sensor_values.append(sensor_value_converted)
+            sensor_values.get()
+            sensor_values.put(sensor_value_converted)
             time.sleep((readin_timestep_in_ms / number_of_values_to_readin) / 100)
 
-        print(sensor_values)
+        print(list(sensor_values.queue))
+
+
+"!!!average calculating extend and correct with queue -> threading with both functions and one list possible"
 
 
 def average_value(values_list):
-    number_to_divide = len(values_list)
-    average_value = sum(values_list) / number_to_divide
-    print(average_value)
-    return average_value
+    while True:
+        number_to_divide = len(values_list)
+        if number_to_divide > 0:
+            average_value = sum(values_list) / number_to_divide
+            print(average_value)
+            return average_value
 
 
 values_to_process = threading.Thread(
@@ -44,5 +50,7 @@ values_to_process = threading.Thread(
     args=(sensor.sensor_line, "mid", 200, 10),
 )
 
+# calculate_average = threading.Thread(target=average_value, args=sensor_values)
 
 values_to_process.start()
+# calculate_average.start()
